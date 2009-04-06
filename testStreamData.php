b9a026c4e82493b23d41e811d72e7cd3b1e07431
Chris Goffinet <goffinet@digg.com>
2009-04-05 23:24:49 -0700

    Updated image handler to image/gif.

--- testStreamData.php
+++ testStreamData.php
@@ -22,7 +22,7 @@ $imgFile = file_get_contents('img/32x32-digg-guy.gif');
 
 for($i = 0; $i < 300 ; $i++) {
     echo "--|||
-Content-Type: image/jpeg
+Content-Type: image/gif
 " . base64_encode($imgFile);
 }
 
